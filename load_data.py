070e9af08aca58d4de7680f37f9bbd7d646c3919
Ditshego-Xobo <ditshegodiks@gmail.com>
2026-05-23 19:50:26 +0200

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -16,7 +16,7 @@ def connect_to_database():
         host="localhost",
         database="karoo_capstone",
         user="postgres",
-        password="Masobo@2025",
+        password="******",
         port="5432"
     )
     return conn
